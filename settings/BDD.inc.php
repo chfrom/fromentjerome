180b375a6ce8ac95f5a216067702976c4ee5136b
chfrom <jerome.froment62130@gmail.com>
2016-12-18 17:41:31 +0100

    Update BDD.inc.php

--- settings/BDD.inc.php
+++ settings/BDD.inc.php
@@ -1,7 +1,7 @@
 <?php
 $connect=false;
 try {
-    $bdd=new PDO('mysql:host=mysql.hostinger.fr;dbname=u653518821_frome;charset=utf8','u653518821_frome','Napoleon-1');//connexion de la BDD
+    $bdd=new PDO('mysql:host=mysql.hostinger.fr;dbname=u6535121_frome;charset=utf8','u6535121_frome','MDP');//connexion de la BDD
     $bdd->exec("set names utf8");//init du utf8
     $bdd->setAttribute(PDO::ATTR_ERRMODE,  PDO::ERRMODE_EXCEPTION);
 } catch (Exception $e) {
